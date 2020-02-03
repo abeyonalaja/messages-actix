0a8e13b546043d0bf85731bc711894e9d03b4ff8
Abey Onalaja <Abey.onalaja@gmail.com>
2020-02-03 03:08:25 +0000

    Add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use messages_actix::MessageApp;
+
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    let app = MessageApp::new(8080);
     println!("Hello, world!");
 }
